b74dceef63f4ff8955955f1c9c1d912a8820152f
Kirankumar Patel <kiran-kumar.patel@atos.net>
2025-09-23 23:36:54 +0530

    added php website folder

--- Slave_Node_Setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo dnf update -y
-sudo dnf install git -y
-sudo dnf install java-21-amazon-corretto -y
-sudo dnf install python3 -y
-sudo dnf install pyhton3-pip -y
-sudo dnf install openssh-server -y
-sudo systemctl enable --now sshd
-sudo dnf update -y
